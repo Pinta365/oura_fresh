9336565f14b2d25b6d756483576e946075f3e46e
Pinta365 <anders@billfors.com>
2023-09-07 22:25:02 +0200

    local copy of pico css

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,10 +8,7 @@ export default function App({ Component }: AppProps) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>oura_fresh</title>
                 <link rel="stylesheet" href="/styles.css" />
-                <link
-                    rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css"
-                />
+                <link rel="stylesheet" href="/pico.min.css"/>
             </head>
             <body>
                 <div class="container">
